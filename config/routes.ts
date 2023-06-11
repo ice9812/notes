5f82d496eabc7de35aedc555f186cd815989d67c
ice <74278245+ice9812@users.noreply.github.com>
2023-06-11 16:39:27 +0800

    fix: init

---
+++ config/routes.ts
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path: '/',
+        redirect: '/home',
+    },
+    {
+        name: '首页',
+        path: '/home',
+        component: './Home',
+    },
+
+]
+
+export default routes
